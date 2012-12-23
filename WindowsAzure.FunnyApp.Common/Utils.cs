e55a77943416886affd3560e098c0c43bf6b95bb
İbrahim ATAY <info@ibrahimatay.org>
2012-12-23 20:39:42 +0200

    Common layer

---
+++ WindowsAzure.FunnyApp.Common/Utils.cs
@@ -0,0 +1,10 @@
+﻿namespace WindowsAzure.FunnyApp.Common
+{
+    public class Utils
+    {
+        public const string ConfigurationString = "DataConnectionString";
+
+        public const string CloudQueueKey = "Imagequeue";
+        public const string CloudBlobKey = "Imageblob";
+    }
+}
